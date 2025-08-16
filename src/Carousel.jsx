e860929371355da20cd1577113d77e6e863ca2e1
Mupenzi Herve <mupenziherve04@gmail.com>
2025-08-16 12:34:47 +0200

    css

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -4,16 +4,21 @@ import {FaQuoteRight} from 'react-icons/fa'
 import {FiChevronLeft, FiChevronRight} from 'react-icons/fi'
 
 const Carousel = () => {
-     const [people, setPeople] = useState (shortList);
+     const [people, setPeople] = useState (list);
+     const [currentPerson, setCurrentPerson] = useState(0);
      const prevSlide = () => {};
      const nextSlide = () => {};
   return (
     <section className="slider-container">
-        {people.map((person)=>{
+        {people.map((person,peopleIndex)=>{
           const {id, image, name, title, quote} = person;
             return (
 
-              <article className="slide" key={id}>
+              <article className="slide" 
+              style={{ transform:
+               `translateX(${100* (personIndex - currentPerson) }%)`,
+                }}
+                 key={id}>
                 <img src={image} alt={name} className="person-img"/>
                 <h5 className="name">{name}</h5>
                 <p className="title">{title}</p>
