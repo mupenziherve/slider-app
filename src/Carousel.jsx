1abd35aad0cb0d265e809c2bc41cc235f5bafd37
Mupenzi Herve <mupenziherve04@gmail.com>
2025-08-17 14:24:49 +0200

    he we go

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,24 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { shortList, list, longList } from "./data";
 import {FaQuoteRight} from 'react-icons/fa'
 import {FiChevronLeft, FiChevronRight} from 'react-icons/fi'
 
 const Carousel = () => {
-     const [people, setPeople] = useState (list);
+     const [people, setPeople] = useState (longList);
      const [currentPerson, setCurrentPerson] = useState(0);
-     const prevSlide = () => {};
-     const nextSlide = () => {};
+
+     const prevSlide = () => {
+      setCurrentPerson ((oldPerson) =>{
+        const result = (oldPerson - 1 + people.length) % people.length;
+        return result;
+      });
+     };
+     const nextSlide = () => {
+      setCurrentPerson ((oldPerson) =>{
+        const result = (oldPerson + 1) % people.length;
+        return result;
+      });
+     };
+
+     useEffect(()=>{
+      let sliderId = setInterval(()=>{nextSlide();},2000);
+      return () => {
+        clearInterval(sliderId);
+      }
+     },[])
   return (
     <section className="slider-container">
-        {people.map((person,peopleIndex)=>{
+        {people.map((person,personIndex)=>{
           const {id, image, name, title, quote} = person;
             return (
 
               <article className="slide" 
               style={{ transform:
                `translateX(${100* (personIndex - currentPerson) }%)`,
+               opacity: personIndex === currentPerson ? 1 : 0,
+                visibility: personIndex === currentPerson ? 'visible': 'hidden',
                 }}
-                 key={id}>
+                 key={id}
+                 >
                 <img src={image} alt={name} className="person-img"/>
                 <h5 className="name">{name}</h5>
                 <p className="title">{title}</p>
